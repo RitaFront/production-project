88c49e9bc7f8b8933250d3b1d59d5e2076d7ff5f
margarita <ritatokareva99@mail.ru>
2023-12-13 18:30:10 +0300

    add architecture

---
+++ src/app/App.tsx
@@ -0,0 +1,26 @@
+import React, { Suspense } from 'react';
+import './styles/index.scss';
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+
+export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle</button>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/'} element={<MainPage />} />
+          <Route path={'/about'} element={<AboutPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
---
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+import React, { FC, useMemo, useState } from 'react';
+import { Theme, ThemeContext } from '../lib/ThemeContext';
+
+const defaultTheme =
+  (localStorage.getItem('theme') as Theme) || Theme.LIGHT;
+
+export const ThemeProvider: FC = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const defaultProps = useMemo(
+    () => ({
+      theme: theme,
+      setTheme: setTheme,
+    }),
+    [theme]
+  );
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
