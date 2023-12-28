b654e2ce7d398a9df4a963b0369ba82bd0b84cfe
margarita <ritatokareva99@mail.ru>
2023-12-28 14:10:04 +0300

    stylelint, i18n plugin для линта

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
-import './styles/index.scss';
-import { Suspense } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
-import { useTheme } from './providers/ThemeProvider';
-import { AppRouter } from './providers/router';
+import "./styles/index.scss";
+import { Suspense } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { useTheme } from "./providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
 
 export const App = () => {
   const { theme } = useTheme();
-
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
         <div className="content-page">
--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import styles from './langSwitcher.module.scss';
+import React from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTranslation } from "react-i18next";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
 
 interface LangSwitcherProps {
   className?: string;
@@ -12,16 +11,16 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
   const { t, i18n } = useTranslation();
 
   const toggle = () => {
-    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
   };
 
   return (
     <Button
       theme={ThemeButton.CLEAR}
       onClick={toggle}
-      className={classNames(styles.langSwitcher, {}, [className])}
+      className={classNames("", {}, [className])}
     >
-      {t('Lang')}
+      {t("Lang")}
     </Button>
   );
 };
